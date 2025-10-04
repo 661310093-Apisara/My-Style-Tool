c483aa9edac827ecbc125cf94b99d46189944253
661310073 Pichayatida <littlewaking001@gmail.com>
2025-10-04 10:49:08 +0700

    add draf ui

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-from PySide6 import QtCore, QtGui ,QtWidgets
+try:
+	from PySide6 import QtCore, QtGui ,QtWidgets
+	from shiboken6 import wrapInstance
+except :
+	from PySide2 import QtCore,QtGui,QtWidgets
+	from shiboken2 import wrapInstance
+import maya.OpenMayaUI as omui
+
+class MyStyleToolDialog(QtWidgets.QDialog):
+	def __init__(self,parent=None):
+		super().__init__(parent)
+
+		self.setWindowTitle('My Tool')
+		self.resize(300,300)
+
+		self.mainLayout = QtWidgets.QVBoxLayout()
+		self.setLayout(self.mainLayout)
+
+		self.nameLayout = QtWidgets.QHBoxLayout()
+		self.mainLayout.addLayout(self.nameLayout)
+		self.nameLabel = QtWidgets.QLabel('Name : ')
+		self.nameLineEdit = QtWidgets.QLineEdit()
+		self.nameLayout.addWidget(self.nameLabel)
+		self.nameLayout.addWidget(self.nameLineEdit)
+
+		self.buttonLayout = QtWidgets.QHBoxLayout()
+		self.mainLayout.addLayout(self.buttonLayout)
+		self.CreateButton = QtWidgets.QPushButton('Create')
+		self.CancelButton = QtWidgets.QPushButton('Cancel')
+		self.buttonLayout.addWidget(self.CreateButton)
+		self.buttonLayout.addWidget(self.CancelButton)
+
+		self.mainLayout.addStretch()
+
+
+
+
+def run():
+	global ui
+	try:
+		ui.close()
+	except:
+		pass
+	ptr = wrapInstance(int(omui.MQtUtil.mainWindow()),QtWidgets.QWidget)
+	ui = MyStyleToolDialog(parent=ptr)
+	ui.show()
